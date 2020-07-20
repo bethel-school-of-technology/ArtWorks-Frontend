69536ce3fb091170719663a65bf723c4bd635e62
Irene Kim <irenekim65@gmail.com>
2020-07-20 09:55:05 -0500

    completed grid but still not hooked up to db

--- src/App.js
+++ src/App.js
@@ -3,23 +3,23 @@ import ReactDOM from 'react-dom';
 import './App.css';
 
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
-import {NavigationBar} from './components/NavigationBar';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { NavigationBar } from './components/NavigationBar';
 
-import {Home} from './Home';
+import { Home } from './Home';
 import Gallery from './components/Gallery';
 import Signup from './components/Signup';
 import history from "./utils/history";
 
 
-function App() {
+function App () {
     return (
         <React.Fragment>
             <Router history={history}>
                 <NavigationBar />
                 <Switch>
                     <Route exact path="/" component={Home} />
-                    <Route path="/about" component={Gallery} />
+                    <Route path="/gallery" component={Gallery} />
                     <Route path="/submit" component={Signup} />
                 </Switch>
             </Router>
@@ -29,6 +29,6 @@ function App() {
 
 fetch('/users')
     .then(response => response.text())
-    .then(data => console.log({data}));
+    .then(data => console.log({ data }));
 
 export default App;
